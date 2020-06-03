cef5eedddb045d4e4aa2a4f1967bbc3d0372ccc9
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-06-03 19:44:41 -0300

    search and select forms in create point

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -3,7 +3,9 @@
 
 function populateUFs(){
     const ufSelect = document.querySelector("select[name=uf]");
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+   
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+    
     .then( res => res.json() )
     .then( states => {
         for(const state of states) {
@@ -12,28 +14,63 @@ function populateUFs(){
         
     })
 }
+populateUFs()
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("select[name=state]")
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
     
     const ufValue = event.target.value
 
-    const indexState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexState].text
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
      
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33|35/municipios`
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+ 
 
     fetch(url)
-    .then(res => res.json())
-    .then(cities => {
+    .then( res => res.json())
+    .then( cities => {
+
         for(const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         citySelect.disabled = false        
     } )
     
 }
-populateUFs()
+
 
 changeUF =document.querySelector("select[name=uf]")
 changeUF.addEventListener('change', getCities)
+
+
+
+const itenstoCollect = document.querySelectorAll(".items-grid li")
+const collectedItems =  document.querySelector("input[name=items]")
+let selectedItems = []
+
+for(const item of itenstoCollect){
+    item.addEventListener("click", handleSelectItem)
+}
+
+
+function handleSelectItem(event){
+    const itemLi = event.target 
+    //add and remove class
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => item === itemId)
+    
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter( item => item != itemId)
+        selectedItens = filteredItems
+    }else{
+        selectedItems.push(itemId)
+    }
+    collectedItems.value = selectedItems
+    
+}
