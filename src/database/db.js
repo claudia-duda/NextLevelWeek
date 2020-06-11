13c11a60cd003f2c3595e5c618c7d927e439c7d7
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-06-11 13:52:06 -0300

    Day 5 - database,create points and search results

---
+++ src/database/db.js
@@ -0,0 +1,70 @@
+const sqlite3 = require("sqlite3").verbose()
+const db = new sqlite3.Database("./src/database/database.db")
+
+//using data base
+/* db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name, TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );        
+    `)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    
+    const values = [
+        "https://images.unsplash.com/photo-1528190336454-13cd56b45b5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "Colectoria",
+        "Rio doce, 1 etapa",
+        "numero 50",
+        "Rio Doce",
+        "Rio do sul",
+        "Resíduos Eletrônicos, Lâmpadas"
+    ]
+
+    function afterInsertData(err) {
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+    //db.run(query, values, afterInsertData) inserir o dado
+    
+
+    db.run(`DELETE FROM places WHERE id = ?`,[2],function(err){
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Registro deletado")
+    })
+    
+    db.all(`SELECT name FROM places`, function(err,rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+    })
+
+}) */
